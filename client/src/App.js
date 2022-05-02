d04161e62b6649d06950215e6541fb42ca7ed66e
Vili Raunola <raunolavili@gmail.com>
2022-05-02 19:07:14 +0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Sidebar from './components/Sidebar';
+import Home from './pages/Home'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Sidebar/>
+        <Routes>
+          <Route path='/' exact element={<Home/>}></Route>
+        </Routes>
+    </Router>
+  )
+
+}
+
+export default App;
