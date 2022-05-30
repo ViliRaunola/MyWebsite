f88d0a788bebb0d86f7808438d2b104b6867f038
Vili Raunola <raunolavili@gmail.com>
2022-05-30 19:57:03 +0300

    message sending added + popup for success

--- client/src/components/ContactSection.js
+++ client/src/components/ContactSection.js
@@ -1,9 +1,66 @@
 import React from 'react'
 import './ContactSection.css'
 import {GoLocation, GoDeviceMobile, GoMail} from 'react-icons/go'
+import emailjs from '@emailjs/browser';
+import { useState, useEffect } from 'react';
+import {toast} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+import { useForm } from "react-hook-form";
 
+toast.configure()
 
 function ContactSection({reference}) {
+
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+
+  // const initialValues = {name: "", email: "", message: ""};
+  // const [formValues, setFormValues] = useState(initialValues);
+  // const [formErrors, setFormErrors] = useState({});
+  // const [isSubmit, setIsSubmit] = useState(false)
+
+  // const handleChange = (e) => {
+  //   const {name, value} = e.target;
+  //   setFormValues({...formValues, [name]: value});
+  // }
+
+  // const validate = (values) => {
+  //   const errors = {};
+  //   const regex = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/;
+  //   if(!values.name){
+  //     errors.name = 'Name is required';
+  //   }
+  //   if(!values.email){
+  //     errors.email = 'Email is required';
+  //   }else if(!regex.test(values.email)){
+  //     errors.email = 'This is not a valid email'
+  //   }
+  //   if(!values.message){
+  //     errors.message = 'Message is required';
+  //   }
+  //   return errors;
+  // }
+
+  // useEffect(() => {
+  //   if(Object.keys(formErrors).length === 0 && isSubmit){
+      
+  //   }
+  // },[formErrors])
+
+
+  const sendEmail = (data, e) => {
+    e.preventDefault();
+    emailjs.sendForm('service_fez7scn', 'template_o6oj5hk', e.target, '5dozwad2-CnWsYs6c')
+    .then((result) => {
+        console.log(result.text);
+    }, (error) => {
+        console.log(error.text);
+    });
+    console.log('Message sent for me')
+    toast('Message has been sent!')
+    e.target.reset()
+  }
+
+
     return (
       <div ref={reference} className='contactsection'>
           <h1>Contact Me</h1>
@@ -29,27 +86,27 @@ function ContactSection({reference}) {
             </div>
 
             <div className='form-container'>
-
-              <div className='form-group'>
-                <label htmlFor='name'>Your name</label>
-                <input className='form-input' type='text' id='name' ></input>
+              <form onSubmit={ handleSubmit(sendEmail)}>
+                <div className='form-group'>
+                  <label htmlFor='name'>Your name</label>
+                  <input className='form-input' type='text' id='name' name='name' {...register("name", { required: true })}></input>
+                  {errors.name && <p className='error-message'>Name is needed!</p>}
+                </div>
                 
-              </div>
-              
-              <div className='form-group'>
-                <label htmlFor='email'>Your email</label>
-                <input className='form-input' type='text' id='email' ></input>
-                
-              </div>
+                <div className='form-group'>
+                  <label htmlFor='email'>Your email</label>
+                  <input className='form-input' type='text' id='email' name='email' {...register("email", {pattern: /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/,required: true })}></input>
+                  {errors.email && <p className='error-message'>Email is needed!</p>}
+                </div>
 
-              <div className='form-group'>
-                <label htmlFor='text'>Your message</label>
-                <textarea className='form-input-message' type='text' id='text' ></textarea>
-                
-              </div>
+                <div className='form-group'>
+                  <label htmlFor='text'>Your message</label>
+                  <textarea className='form-input-message' type='text' id='text' name='message' {...register("message", { required: true })}></textarea>
+                  {errors.message && <p className='error-message'>Message is needed!</p>}
+                </div>
 
-              <button type='submit'>Send</button>
-
+                <button className='button-3' type='submit'>Send</button>
+              </form>
             </div> 
           </div>
         
