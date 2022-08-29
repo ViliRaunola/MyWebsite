fef8e049244488e63499b73661e2cf794a197948
Vili Raunola <raunolavili@gmail.com>
2022-08-29 16:29:22 +0300

    popup works & icon added to close button

--- client/src/components/Popup.js
+++ client/src/components/Popup.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Popup.css'
 import {useRef, useEffect} from 'react'
+import {GoX} from 'react-icons/go'
 
 function Popup(props) {
 
@@ -27,7 +28,9 @@ function Popup(props) {
     return (props.trigger) ? (
         <div className='popup'>
             <div ref={popupRef} className='popup-inner'>
-                <button className='button-close' onClick={ () => props.setTrigger(false)}>ikoni</button>
+                <button className='button-close' onClick={ () => props.setTrigger(false)} style={{width: '30px', height: '30px', alignItems: 'center', justifyContent: 'center'}}>
+                    <GoX style={{marginBottom: '0.2em', marginRight: '0.3em'}}></GoX>
+                </button>
                 {props.children}
             </div>
         </div>
