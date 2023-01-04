8d61477b3ece1d86b04f4f9bee0c3036248bf2ea
ViliRaunola <61086026+ViliRaunola@users.noreply.github.com>
2023-01-04 18:47:37 +0200

    Server added

---
+++ server/app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.use(express.static(path.resolve("..", "client", "build"))); //Telling the server where to find the front end files to serve
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve("..", "client", "build", "index.html")) //All other requests that are not for the server's router are sent to react that will handel them
+});
+
+module.exports = app;
